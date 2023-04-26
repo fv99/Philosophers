2ca065856dd5a20e59c0f12c60ca8d3d3b76f16b
fv99 <fuck@you.com>
2023-04-26 18:26:50 +0200

    Added timestamp functions

--- philo.c
+++ philo.c
@@ -6,7 +6,7 @@
 /*   By: fvonsovs <fvonsovs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 16:28:13 by fvonsovs          #+#    #+#             */
-/*   Updated: 2023/04/25 16:30:02 by fvonsovs         ###   ########.fr       */
+/*   Updated: 2023/04/26 18:10:36 by fvonsovs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,3 +19,33 @@ int	you_fucked_up(char *msg)
 	exit(1);
 }
 
+unsigned long long timestamp(void)
+{
+	struct timeval	time;
+	gettimeofday(&time, NULL);
+	return(time.tv_sec * 1000 + tv.tv_usec / 1000);
+}
+
+/* 
+	mode 0 - timestamp_in_ms [num] has taken a fork
+	mode 1 - timestamp_in_ms [num] is eating
+	mode 2 - timestamp_in_ms [num] is sleeping
+	mode 3 - timestamp_in_ms [num] is thinking
+	mode 4 - timestamp_in_ms [num] died
+ */
+void	print_status(int mode, int num)
+{
+	const unsigned long long	time;
+
+	time = timestamp();
+	if (mode == 0)
+		printf("%llu %i has taken a fork", time, num);
+	else if (mode == 1)
+		printf("%llu %i is eating", time, num);
+	else if (mode == 2)
+		printf("%llu %i is sleeping", time, num);
+	else if (mode == 3)
+		printf("%llu %i is thinking", time, num);
+	else if (mode == 4)
+		printf("%llu %i died", time, num);
+}
