e7ab4a75943f7ec3f1b0096db14e8e96fccee945
fv99 <filipvon@protonmail.com>
2023-05-04 14:38:38 +0200

    Philosophers now actually dont eat with 1 fork

--- philo.c
+++ philo.c
@@ -6,7 +6,7 @@
 /*   By: fvonsovs <fvonsovs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 16:28:13 by fvonsovs          #+#    #+#             */
-/*   Updated: 2023/05/03 16:21:57 by fvonsovs         ###   ########.fr       */
+/*   Updated: 2023/05/04 14:35:48 by fvonsovs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,43 +20,43 @@ void	*philo_routine(void *arg)
 	philo = (t_philo *)arg;
 	if (philo->id % 2 == 0)
 		ft_usleep(philo->data->t_eat);
+	pthread_create(&thread, NULL, is_dead, philo);
 	while(philo->dead == 0)
 	{
-		pthread_create(&thread, NULL, is_dead, philo);
 		philo_fork(philo);
 		philo_eat(philo);
-		pthread_detach(thread);
 		if (philo->n_ate >= philo->data->n_eat)
-			return(NULL);
+			break;
 		print_status(2, philo->id);
 		ft_usleep(philo->data->t_sleep);
 		print_status(3, philo->id);
 	}
+	pthread_detach(thread);
 	return (NULL);
 }
 
 void	philo_fork(t_philo *philo)
 {
-	pthread_mutex_lock(&philo->left);
-	print_status(0, philo->id);
 	if (philo->data->n_philo == 1)
 	{
-		ft_usleep(philo->data->t_die * 2);
-		return ;
+		ft_usleep(philo->data->t_eat * 2);
+		return;
 	}
+	pthread_mutex_lock(&philo->left);
+	print_status(0, philo->id);
 	pthread_mutex_lock(philo->right);
 	print_status(0, philo->id);
 }
 
 void	philo_eat(t_philo *philo)
 {
-	if (philo->dead == 1)
+	if (philo->dead == 1 || philo->data->n_philo == 1)
 		return ;
 	print_status(1, philo->id);
 	philo->n_ate++;
 	philo->last_ate = timestamp();
-	pthread_mutex_unlock(philo->right);
 	pthread_mutex_unlock(&philo->left);
+	pthread_mutex_unlock(philo->right);
 }
 
 void	*is_dead(void *arg)
@@ -64,12 +64,16 @@ void	*is_dead(void *arg)
 	t_philo *philo;
 
 	philo = (t_philo *)arg;
-	if (timestamp() - philo->last_ate > philo->data->t_die)
+	while (!philo->dead)
 	{
-		print_status(4, philo->id);
-		philo->dead = 1;
-		pthread_mutex_unlock(philo->right);
-		pthread_mutex_unlock(&philo->left);
+		if (timestamp() - philo->last_ate > philo->data->t_die)
+		{
+			print_status(4, philo->id);
+			philo->dead = 1;
+			pthread_mutex_unlock(philo->right);
+			pthread_mutex_unlock(&philo->left);
+		}
+		ft_usleep(10);
 	}
 	return (NULL);
 }
